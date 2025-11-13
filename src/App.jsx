4fc7ed76d20a65ddef78554ad6472820deadeba9
Elechosystem <a.mathieu@elechosystem.com>
2025-11-13 19:03:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1187,7 +1187,7 @@ export default function App() {
             onClick={() => setScreen('superAdminLogin')}
             className="bg-yellow-300 text-yellow-900 px-12 py-8 rounded-2xl text-3xl font-black hover:bg-yellow-200 transition-all shadow-2xl shadow-yellow-500/40 border-4 border-yellow-500"
           >
-            👑 SUPER ADMIN
+            👑 GESTIONNAIRE
           </button>
         </div>
       </div>
